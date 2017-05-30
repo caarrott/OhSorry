86d5ac5b96f1e70f78f63aba970557d09a92cd69
YangJongYeol <yajya004@gmail.com>
2017-05-30 18:00:31 +0900

    Join & LogIn

--- app/src/main/java/com/example/yajya/oh_sorry/Join.java
+++ app/src/main/java/com/example/yajya/oh_sorry/Join.java
@@ -1,21 +1,62 @@
 package com.example.yajya.oh_sorry;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Join extends AppCompatActivity {
+    EditText id;
+    EditText password;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        id = (EditText)findViewById(R.id.ID);
+        password = (EditText)findViewById(R.id.PASSWORD);
+        name = (EditText)findViewById(R.id.NAME);
     }
 
     public void btnSignUp(View view) {
-        Intent intent = new Intent(this, LogIn.class);
-        startActivity(intent);
-        finish();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table = database.getReference("AppDB/User");
+
+        final String Id = id.getText().toString();
+        Query query = table.orderByChild("Id").equalTo(Id);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChildren()){
+                    Toast.makeText(getApplicationContext(),"아이디가 중복됩니다.",Toast.LENGTH_SHORT).show();
+                } else {
+                    DatabaseReference user = table.push();
+                    user.child("Id").setValue(Id);
+                    user.child("Password").setValue(password.getText().toString());
+                    user.child("Name").setValue(name.getText().toString());
+
+                    Intent intent = new Intent(getApplicationContext(), LogIn.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/yajya/oh_sorry/LogIn.java
+++ app/src/main/java/com/example/yajya/oh_sorry/LogIn.java
@@ -4,13 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LogIn extends AppCompatActivity {
+    EditText id;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        id = (EditText)findViewById(R.id.ID);
+        password = (EditText)findViewById(R.id.PASSWORD);
     }
 
     public void Join(View view) {
@@ -20,8 +34,44 @@ public class LogIn extends AppCompatActivity {
     }
 
     public void Login(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        String Id = id.getText().toString();
+        final String Password = password.getText().toString();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table = database.getReference("AppDB/User");
+
+        Query query = table.orderByChild("Id").equalTo(Id);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChildren()){
+                    Query query1 = table.orderByChild("Password").equalTo(Password);
+                    query1.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.hasChildren()){
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "일치하는 아이디가 없습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
