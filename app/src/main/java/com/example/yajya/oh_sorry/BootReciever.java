e8f2aa153d883f304c26ed68da6cb48b61fc97f4
jone1222 <jone1222@hanmail.net>
2017-06-05 20:01:35 +0900

    AutoStart when device has completely booted
    Hou!

---
+++ app/src/main/java/com/example/yajya/oh_sorry/BootReciever.java
@@ -0,0 +1,25 @@
+package com.example.yajya.oh_sorry;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+/**
+ * Created by JSK on 2017-06-05.
+ */
+
+public class BootReciever extends BroadcastReceiver {
+    SharedPreferences settings;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        settings = context.getSharedPreferences("setting",0);
+        if(settings.getBoolean("useAutoStart",false)) {
+            if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+
+                Intent i = new Intent(context, myService.class);
+                context.startService(i);
+            }
+        }
+    }
+}
